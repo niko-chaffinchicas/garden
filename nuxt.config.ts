65be80fcf0aad4d84f3fa817d75912bd9db4f2d0
Niko Chaffin-Chicas <135640725+niko-chaffinchicas@users.noreply.github.com>
2025-12-06 20:22:49 -0800

    feat: set app base url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
+  app: {
+    baseUrl: '/garden/'
+  },
+
   modules: [
     '@nuxt/content',
     '@nuxt/fonts',
