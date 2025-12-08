e7870c0dc1e37a2e7a8a7dd66e7acd7a6957876e
Niko Chaffin-Chicas <niko.chaffinchicas@gmail.com>
2025-12-08 13:51:17 -0800

    fix: add missing component

---
+++ app/components/PageHeader.vue.vue
@@ -0,0 +1,14 @@
+<template>
+  <pre>{{ page.meta.date }}</pre>
+  <pre>{{ stages[page.meta.stage as number] }}</pre>
+</template>
+
+<script setup lang="ts">
+import type { ContentCollectionItem } from "@nuxt/content";
+
+const { page } = defineProps<{
+  page: ContentCollectionItem;
+}>();
+
+const stages = ["scribble", "draft", "review", "published", "archived"];
+</script>
