51dc1c413fbacf879fe89697764d56820cf93338
Niko Chaffin-Chicas <niko.chaffinchicas@gmail.com>
2025-12-10 15:24:25 -0800

    feat: add initial font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
+  css: ['~/assets/styles/main.css'],
+
   app: {
     baseURL: '/garden/'
   },
