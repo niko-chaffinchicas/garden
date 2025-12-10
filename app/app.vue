a4d22ce5b003daa590cba70ec58ea670c942c8a0
Niko Chaffin-Chicas <niko.chaffinchicas@gmail.com>
2025-12-09 21:05:58 -0800

    feat: add rudimentary navigation

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,6 @@
 <template>
   <NuxtLayout>
+    <NavBar />
     <NuxtPage />
   </NuxtLayout>
 </template>
---
+++ app/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <ul>
+      <li v-for="link of navigation" :key="link.path">
+        <NuxtLink :to="link.path">{{ link.title }}</NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script setup lang="ts">
+const { data: navigation } = await useAsyncData(() =>
+  queryCollectionNavigation("content")
+);
+</script>
